a9c2acdecdb3310c1216d574ad28481eaf641b77
iamsns <shivn4128@gmail.com>
2022-06-14 01:22:53 +0530

    Updates

---
+++ precache-manifest.96d8931ba9277267d3e80a886416b88e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e09611e9764e857ae17c",
+    "url": "/table-task/static/css/main.7bccfacc.chunk.css"
+  },
+  {
+    "revision": "e09611e9764e857ae17c",
+    "url": "/table-task/static/js/main.e09611e9.chunk.js"
+  },
+  {
+    "revision": "23172ffde8cf25f62d55",
+    "url": "/table-task/static/js/1.23172ffd.chunk.js"
+  },
+  {
+    "revision": "7bbb23c3020cad904979",
+    "url": "/table-task/static/css/2.17219317.chunk.css"
+  },
+  {
+    "revision": "7bbb23c3020cad904979",
+    "url": "/table-task/static/js/2.7bbb23c3.chunk.js"
+  },
+  {
+    "revision": "2709016a242300b7a694",
+    "url": "/table-task/static/js/runtime~main.2709016a.js"
+  },
+  {
+    "revision": "8ad4e618c54e95d18b7e1c414d6a8942",
+    "url": "/table-task/index.html"
+  }
+];
